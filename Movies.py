ef61d18b8dfa14f48e66aafe3dc3cc0c2bfe85e1
Rakesh <rockey0508@gmail.com>
2017-10-17 17:39:25 -0600

    all

--- Movies.py
+++ Movies.py
@@ -1,2 +1,2 @@
-telugu=['Okkadu','athidi','Dookudu','Srimathudu','Bussiness man','Arjun reddy','1 nenuokadina','Spyder']
+telugu=['Okkadu','athidi','Dookudu','Srimathudu','Bussiness man','Arjun reddy','1 nenuokadina','Spyder','titanic','don']
 print telugu
--- dict.py
+++ dict.py
@@ -1,4 +1,4 @@
 def movies:
-    mv=dict{'telgu':25,'tamil':26,'hindi':27,'English':28}
+    mv=dict{'telgu':25,'tamil':26,'hindi':27,'English':28,'Chinese':29}}
     return mv
 print range.mv(0:)    
